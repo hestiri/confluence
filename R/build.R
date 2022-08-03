cfc376d08a3265b688fb7fbb82d1bf12701116fd
hestiri <estiri.hossein@gmail.com>
2022-08-03 11:46:56 -0400

    8.3.1

--- R/build.R
+++ R/build.R
@@ -1,2 +1,10 @@
 # An optional custom script to run before Hugo builds your site.
 # You can delete it if you do not need it.
+
+library(blogdown)
+##template download
+# blogdown::new_site(theme = "opera7133/tella")
+
+##build and serve site locally
+# blogdown::build_site()
+# blogdown::serve_site()
